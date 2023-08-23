b35d8e4e73d86736cca1b2b1bc447e2316d22260
Venera Nechaeva <venera_nechaeva@mail.ru>
2023-08-23 17:45:49 +0300

    feat: восстановить функциональность приложения

--- backend/app.js
+++ backend/app.js
@@ -41,7 +41,7 @@ const routerUser = require('./routes/users');
 const routerCard = require('./routes/cards');
 
 // Слушаем 3000 порт
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 // Cоздание приложения методом express
 const app = express();
 
